fdce85da816d3a09b5aae6523aeef8dcdd75cfb6
TalisonF <talison737@gmail.com>
2019-09-16 13:20:03 -0300

    Up arquivos

---
+++ src/aps/Treinamento.java
@@ -0,0 +1,63 @@
+
+package aps;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.nio.IntBuffer;
+import static org.bytedeco.javacpp.opencv_core.CV_32SC1;
+import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.MatVector;
+import org.bytedeco.javacpp.opencv_core.Size;
+import org.bytedeco.javacpp.opencv_face.FaceRecognizer;
+import static org.bytedeco.javacpp.opencv_face.createEigenFaceRecognizer;
+import static org.bytedeco.javacpp.opencv_face.createFisherFaceRecognizer;
+import static org.bytedeco.javacpp.opencv_face.createLBPHFaceRecognizer;
+import static org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
+import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+import static org.bytedeco.javacpp.opencv_imgproc.resize;
+
+/**
+ * @author TI
+ */
+public class Treinamento {
+    public static void main(String args[]){
+        File diretorio = new File("src\\fotos");
+        FilenameFilter filtroImagem = new FilenameFilter(){
+            @Override
+            public boolean accept(File dir, String nome){
+                return nome.endsWith(".jpg");
+            }
+        };
+        
+        File[] arquivos = diretorio.listFiles(filtroImagem);
+        MatVector fotos = new MatVector(arquivos.length);
+        Mat rotulos = new Mat(arquivos.length, 1,CV_32SC1);
+        IntBuffer rotulosBuffer = rotulos.createBuffer();
+        int contador = 0; 
+        for(File imagem: arquivos){
+            Mat foto = imread(imagem.getAbsolutePath(), CV_LOAD_IMAGE_GRAYSCALE);
+            int classe = Integer.parseInt(imagem.getName().split("\\.")[1]);
+            System.out.println(classe);
+            resize(foto,foto, new Size(160,160));
+            fotos.put(contador, foto);
+            rotulosBuffer.put(contador,classe);
+            contador ++;
+        }
+        
+        FaceRecognizer eigenfaces = createEigenFaceRecognizer();
+        FaceRecognizer fisherFaces = createFisherFaceRecognizer();
+        FaceRecognizer lbph = createLBPHFaceRecognizer();
+        System.out.println("Treinamento iniciado");
+        eigenfaces.train(fotos, rotulos);
+        eigenfaces.save("src\\recursos\\classificadorEigenFaces.yml");
+        System.out.println("Treinamento Eigen Face Realizado");
+        
+        fisherFaces.train(fotos, rotulos);
+        eigenfaces.save("src\\recursos\\classificadorFisherFaces.yml");
+        System.out.println("Treinamento Fisher Face Realizado");
+        
+        lbph.train(fotos, rotulos);
+        lbph.save("src\\recursos\\classificadorLBPHFaces.yml");
+        System.out.println("Treinamento LBPH Face Realizado");
+    }
+}
